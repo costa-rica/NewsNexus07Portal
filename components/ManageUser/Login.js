21b807106eca3720a0f9a5389f983937955778a9
Nick <nrodrig1@gmail.com>
2025-04-11 17:14:16 +0200

    InputPassword component nicely fixed

--- components/ManageUser/Login.js
+++ components/ManageUser/Login.js
@@ -1,12 +1,51 @@
-import styles from "../../styles/Home.module.css";
-
+import styles from "../../styles/ManageUser.module.css";
+import InputPassword from "../common/InputPassword";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 export default function Login() {
+  const [email, emailSetter] = useState("");
+  const [password, passwordSetter] = useState("");
+  // const dispatch = useDispatch();
+  const router = useRouter();
+  // const userReducer = useSelector((state) => state.user.value);
+  const sendPasswordBackToParent = (passwordFromInputPasswordElement) => {
+    passwordSetter(passwordFromInputPasswordElement);
+  };
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Login Page</a>
-        </h1>
+        <div className={styles.divLeft}>
+          <img
+            className={styles.imgKmLogo}
+            src="/images/logoWhiteBackground.png"
+            alt="NewsNexus Logo"
+          />
+          <h1 className={styles.h1PageTitle}>Login</h1>
+
+          <div className={styles.divEmailAndPassword}>
+            <div className={styles.divInputGroup}>
+              <label htmlFor="email">Email</label>
+              <input
+                className={styles.inputEmail}
+                type="email"
+                placeholder="example@gmail.com"
+              />
+            </div>
+            <div className={styles.divInputGroup}>
+              <label htmlFor="password">Password</label>
+              <InputPassword
+                sendPasswordBackToParent={sendPasswordBackToParent}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.divRight}>
+          <img
+            className={styles.imgKmLogo}
+            src="/images/kmLogo_square1500.png"
+            alt="Km Logo"
+          />
+        </div>
       </main>
     </div>
   );
