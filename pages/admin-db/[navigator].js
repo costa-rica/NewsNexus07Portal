82c7a39e43e693f5923f8b3900a9afc506748fff
Nick <nrodrig1@gmail.com>
2025-04-12 09:19:26 +0200

    navigation works pretty well - commiting before trying a universal [navigator] name for all dynamic routes

---
+++ pages/admin-db/[navigator].js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+
+export default function Navigator() {
+  const router = useRouter();
+  const { navigator } = router.query;
+
+  const NavigatorComponent = dynamic(() => {
+    if (navigator === "manage-db-backups") {
+      return import(`../../components/AdminDb/ManageDbBackups`).catch(
+        () => () => <p>ManageDbBackups Not Found</p>
+      );
+    } else {
+      return Promise.resolve(() => <p>Something else Not Found</p>);
+    }
+  });
+
+  return <NavigatorComponent />;
+}
