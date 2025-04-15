ba6c8c9c0117b2c7ae33364a8b75ffbb8536a119
Nick <nrodrig1@gmail.com>
2025-04-15 13:31:12 +0200

    approve and un-approve button - with content modifieable

--- components/Articles/ReviewArticles.js
+++ components/Articles/ReviewArticles.js
@@ -12,10 +12,13 @@ export default function ReviewArticles() {
   const userReducer = useSelector((state) => state.user);
   const [articlesArray, setArticlesArray] = useState([]);
   const [stateArray, setStateArray] = useState(userReducer.stateArray);
-  const [isOpenApproveModal, setIsOpenApproveModal] = useState(false);
+  // const [isOpenApproveModal, setIsOpenApproveModal] = useState(false);
+  const [isOpenModalInfo, setIsOpenModalInfo] = useState(false);
+  const [infoModalContent, setInfoModalContent] = useState("");
   const [isOpenStateWarning, setIsOpenStateWarning] = useState(false);
   const [hideIrrelevant, setHideIrrelevant] = useState(false);
   const [selectedArticle, setSelectedArticle] = useState(null);
+
   useEffect(() => {
     fetchArticlesArray();
     // fetchStateArray();
@@ -52,10 +55,52 @@ export default function ReviewArticles() {
     }
   };
 
-  const handleRowClick = (article) => {
+  // const handleRowClick = async (article) => {
+  const handleSelectArticleFromTable = async (article) => {
     console.log("Selected article:", article);
-    setSelectedArticle(article);
-    updateStateArrayWithArticleState(article);
+
+    // STEP 1: Call API to see if an approved record for this article exists
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/articles/get-approved/${article.id}`,
+        {
+          headers: { Authorization: `Bearer ${userReducer.token}` },
+        }
+      );
+
+      console.log(`Response status: ${response.status}`);
+
+      if (!response.ok) {
+        const errorText = await response.text(); // Log response text for debugging
+        throw new Error(`Server Error: ${errorText}`);
+      }
+
+      const result = await response.json();
+      console.log("Fetched Data (/articles/get-approved):", result);
+
+      if (result.article && result.article.id) {
+        setSelectedArticle({
+          ...result.article,
+          ...article,
+          approved: result.result,
+          content: result.content,
+        });
+        updateStateArrayWithArticleState(result.article);
+      } else {
+        setSelectedArticle({ ...article, content: article.description });
+      }
+      // else {
+      //   console.log("----- blanking out the aritlces list");
+      //   setArticlesArray([]);
+      // }
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+      // setArticlesArray([]);
+    }
+
+    // // STEP 2
+    // setSelectedArticle(article);
+    // updateStateArrayWithArticleState(article);
   };
 
   const handleClickIsRelevant = async (articleId) => {
@@ -127,7 +172,7 @@ export default function ReviewArticles() {
       enableSorting: true,
       cell: ({ row }) => (
         <button
-          onClick={() => handleRowClick(row.original)}
+          onClick={() => handleSelectArticleFromTable(row.original)}
           style={{
             fontSize: "10px",
           }}
@@ -180,10 +225,9 @@ export default function ReviewArticles() {
       enableSorting: true,
       cell: ({ getValue, row }) => (
         <div className={styles.divBtnRelevant}>
-          {/* {getValue() === true ? (<button>Yes</button>) : "No"} */}
           <button
             className={`${styles.btnRelevant} ${
-              getValue() === false ? styles.btnIsNotRelevant : ""
+              getValue() === false ? "btnOpaque" : ""
             }`}
             onClick={() => handleClickIsRelevant(row.original.id)}
           >
@@ -239,67 +283,191 @@ export default function ReviewArticles() {
     }
   };
 
+  const handleApproveArticle = async () => {
+    console.log("clicked approve article");
+    if (!selectedArticle.id) {
+      setRequestResponseMessage("Please select an article to approve");
+      setIsOpenModalInfo(true);
+      return;
+    }
+    try {
+      const bodyObj = {
+        isApproved: !selectedArticle.isApproved,
+        headlineForPdfReport: selectedArticle.title,
+        publicationNameForPdfReport: selectedArticle.publicationName,
+        publicationDateForPdfReport: selectedArticle.publishedDate,
+        textForPdfReport: selectedArticle.content,
+        urlForPdfReport: selectedArticle.url,
+        kmNotes: "",
+      };
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/articles/approve/${selectedArticle.id}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${userReducer.token}`,
+          },
+          body: JSON.stringify(bodyObj),
+        }
+      );
+
+      console.log(`Response status: ${response.status}`);
+      let resJson = null;
+      const contentType = response.headers.get("Content-Type");
+
+      if (contentType?.includes("application/json")) {
+        resJson = await response.json();
+      }
+
+      if (resJson) {
+        console.log("Fetched Data:", resJson);
+        if (response.status === 400) {
+          setRequestResponseMessage(resJson.message);
+          setIsOpenRequestResponse(true);
+          return;
+        } else {
+          //modify selected article
+          setSelectedArticle({
+            ...selectedArticle,
+            isApproved: !selectedArticle.isApproved,
+          });
+          let tempArray = [...articlesArray];
+          tempArray = tempArray.map((article) => {
+            if (article.id === selectedArticle.id) {
+              return {
+                ...article,
+                isApproved: !article.isApproved,
+              };
+            }
+            return article;
+          });
+          setArticlesArray(tempArray);
+        }
+      }
+    } catch (error) {
+      console.error("Error approving article:", error.message);
+    }
+  };
   return (
     <TemplateView>
       <main className={styles.main}>
-        <div className={styles.divMainTop}>
-          Some tables with counts will go here
-          <h1 className={styles.title}>Welcome to Review articles page</h1>
-        </div>
+        <div className={styles.divMainTop}>Articles review: [some number]</div>
         <div className={styles.divMainMiddle}>
           <div className={styles.divMainMiddleLeft}>
-            <div className={styles.divMainMiddleLeftTitle}>
+            <div className={`${styles.divMainMiddleLeftTitle} tooltipWrapper`}>
               <h2>Article Approval Details</h2>
-              <span>(This is the content for CPSC report PDF)</span>
+              <div className="tooltipText">
+                This is the content for CPSC report PDF
+              </div>
             </div>
             <div className={styles.divArticleDetail}>
-              <span className={styles.lblArticleDetail}>Article Title:</span>{" "}
-              <span> {selectedArticle?.title}</span>
+              <div className="tooltipWrapper">
+                <span className={styles.lblArticleDetailMain}>Headline:</span>
+                <span className="tooltipText">This is the article title</span>
+              </div>
+              <input
+                type="text"
+                value={selectedArticle?.title}
+                className={styles.inputArticleDetail}
+                // onChange={(e) =>
+                disabled
+              />
+            </div>
+
+            <div className={styles.divArticleDetail}>
+              <div className="tooltipWrapper">
+                <span className={styles.lblArticleDetailMain}>
+                  Publication:
+                </span>
+                <span className="tooltipText">
+                  Name of newspaper or organization that published the article
+                </span>
+              </div>
+              <input
+                type="text"
+                value={selectedArticle?.publicationName}
+                className={styles.inputArticleDetail}
+                // onChange={(e) =>
+                disabled
+              />
             </div>
             <div className={styles.divArticleDetail}>
-              <span className={styles.lblArticleDetail}>Link to article:</span>{" "}
-              <span>
-                {selectedArticle?.url && (
-                  <Link href={selectedArticle?.url}>
-                    {selectedArticle?.url}
-                  </Link>
-                )}
-              </span>
+              <div className="tooltipWrapper">
+                <span className={styles.lblArticleDetailMain}>
+                  Publication Date:
+                </span>
+                <span className="tooltipText">Date article was published</span>
+              </div>
+              <input
+                type="date"
+                value={selectedArticle?.publishedDate}
+                className={styles.inputArticleDetail}
+                // onChange={(e) =>
+                disabled
+              />
             </div>
-            <div className={styles.divArticleContent}>
-              {selectedArticle?.description}
+            <div className={styles.divArticleDetail}>
+              <div className="tooltipWrapper">
+                <span className={styles.lblArticleDetailMain}>
+                  Article State:
+                </span>
+                <span className="tooltipText">
+                  State(s) article is relevant to
+                </span>
+              </div>
+              <div className={styles.divManageStates}>
+                <StateSelector
+                  stateArray={stateArray}
+                  setStateArray={setStateArray}
+                />
+              </div>
+              <button
+                className={styles.btnSubmit}
+                onClick={() => {
+                  handleClickedValidateState();
+                }}
+              >
+                Validate State
+              </button>
+            </div>
+
+            <div className={styles.divArticleDetailContent}>
+              <div className="tooltipWrapper">
+                <span className={styles.lblArticleDetailMain}>Content:</span>
+                <span className="tooltipText">This is the article content</span>
+              </div>
+              <textarea
+                value={selectedArticle?.content}
+                className={styles.inputArticleDetailContent}
+                onChange={(e) => {
+                  setSelectedArticle({
+                    ...selectedArticle,
+                    content: e.target.value,
+                  });
+                }}
+                // disabled
+              />
             </div>
           </div>
           <div className={styles.divMainMiddleRight}>
-            <div className={styles.divManageStates}>
+            {/* <div className={styles.divManageStates}>
               <StateSelector
                 stateArray={stateArray}
                 setStateArray={setStateArray}
               />
-            </div>
+            </div> */}
 
             <button
-              className={styles.btnSubmit}
+              className={`${styles.btnSubmit} ${
+                selectedArticle?.isApproved ? styles.btnOpaque : ""
+              }`}
               onClick={() => {
-                console.log("approve article");
-                handleClickedValidateState();
-                // setIsOpenApproveModal(true);
-                // handleRequestArticles();
-                // You can call your submit logic or dispatch here
+                // console.log("approve article");
+                handleApproveArticle();
               }}
             >
-              Validate State
-            </button>
-            <button
-              className={styles.btnSubmit}
-              onClick={() => {
-                console.log("approve article");
-                setIsOpenApproveModal(true);
-                // handleRequestArticles();
-                // You can call your submit logic or dispatch here
-              }}
-            >
-              Approve
+              {selectedArticle?.isApproved ? "Un-approve" : "Approve"}
             </button>
 
             <button
@@ -311,6 +479,15 @@ export default function ReviewArticles() {
               {hideIrrelevant
                 ? "Show All Articles"
                 : "Hide Irrelevant Articles"}
+            </button>
+            <button
+              className={styles.btnSubmit}
+              onClick={() => {
+                console.log(selectedArticle);
+                console.log(selectedArticle.isApproved);
+              }}
+            >
+              Show Selected Article
             </button>
           </div>
         </div>
@@ -329,14 +506,21 @@ export default function ReviewArticles() {
           </div>
         </div>
       </main>
-      {isOpenApproveModal && (
+      {/* {isOpenApproveModal && (
         <ModalApproveArticle setIsOpen={setIsOpenApproveModal} />
-      )}
+      )} */}
       {isOpenStateWarning && (
         <Modal
           isModalOpenSetter={setIsOpenStateWarning}
           title="Problem with state request"
           content="Maybe no selected states ?"
+        />
+      )}
+      {isOpenModalInfo && (
+        <Modal
+          isModalOpenSetter={setIsOpenModalInfo}
+          title="Information"
+          content={infoModalContent}
         />
       )}
     </TemplateView>
